765c645887d73d8eed9115a751f2edddbb1598a6
akuzmanov-plume <akuzmanov@plume.com>
2023-11-25 17:50:12 +0100

    Fix node.js dockerfile

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const fs  = require("fs");
 const { MongoClient } = require('mongodb');
 const { Long } = require('bson');
-var axios = require('axios');
 require('dotenv').config();
 
 
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3000;
 
 const mongo_url = process.env.MONGO_URL || "mongodb://localhost:27017";
 const client = new MongoClient(mongo_url);
